4296b495a52fa404839b9f1176a66621d6afe618
Rudeboy Tom <tomperso@yahoo.fr>
2015-04-12 13:59:45 +0200

    Google test require jquery

--- js/koala/build/modules/Connexion/app.js
+++ js/koala/build/modules/Connexion/app.js
@@ -1,49 +1,14 @@
 define(['module'
-        ,'backbone'
         ,'jquery'
-        ,'lib/utils/Store'
-        ,'Connexion/config'
-        ,'Connexion/collections/Users'
-        ,'Connexion/views/AllUsers'
         ],
         
 function(module
-		,Backbone
 		,$
-		,store
-		,conf
-		,Users
-		,AllUsersView
 		 ) {
 	
 	$(document).ready(function() {
 		console.log('yo');
-		var users = new Users();
-		$.when(users.fetch()).then( function() {
-			$('#app').append(new AllUsersView({collection:users}).render().el);
-		}).fail(function() {
-			console.log('fail');
-		});
-		/*
-		var MyApplication = Marionette.Application.extend({
-			regions: {
-				mainRegion: '#app'		
-			},
-			initialize: function(options) {},
-			onStart: function() {
-				this.appView = new AppView();
-				this.mainRegion.show(this.appView);
-			}		
-		});
-		*/
 		
-		
-		
-		
-		/*
-		var myApp = new MyApplication();
-		myApp.start();
-		*/
 	});
 	
 	
